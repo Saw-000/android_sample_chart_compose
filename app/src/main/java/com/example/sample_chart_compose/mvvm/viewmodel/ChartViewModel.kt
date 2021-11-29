045c2e02c47c6e78a7814eb0717c08195ae0a517
Ie So <zousaaaaaan@gmail.com>
2021-11-30 00:55:35 +0900

    display bar chart.

--- app/src/main/java/com/example/sample_chart_compose/mvvm/viewmodel/ChartViewModel.kt
+++ app/src/main/java/com/example/sample_chart_compose/mvvm/viewmodel/ChartViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.sample_chart_compose.mvvm.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.sample_chart_compose.mvvm.model.ChartModel
